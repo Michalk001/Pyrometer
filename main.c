51f91f9c6d9bc257a2c6644265de7c2f64d287b3
michalk001 <kostorz.michal@gmail.com>
2020-06-06 11:11:03 +0200

    approximation data

--- main.c
+++ main.c
@@ -74,7 +74,7 @@ void convertDoubleToCharDisplay(double temperature,char display[7]){
 }
 
 
-void displayTemperature(double temperature, char average[7], char min[7],char max[7]){
+void displayTemperature(double temperature, char average[7]){
 
 	LCD_Clear();
 	LCD_GoTo(0,1);
@@ -86,46 +86,47 @@ void displayTemperature(double temperature, char average[7], char min[7],char ma
 	_delay_ms(100);
 }
 
+
+
 void getData(double Temperatures[21]){
 	
-		int adres = 0xb4;         // Adres czujnika MLX90614
-		int erc = 0;              // Zmienna przechowuj?ca warto?? PEC
-		int daneH = 0;            // Drugi bajt danych
-		int daneL = 0;            // Pierwszy bajt danych
-		double tempnalsb = 0.02;  // Zmienna, przez któr? wymna?ana b?dzie warto?? cyfrowa
-		double temperature = 0;   // Zmienna przechowuj?ca temperatur?
+	int adress = 0xb4;         
+	int erc = 0;              
+	int dateH = 0;           
+	int dataL = 0;            
+	double tempnalsb = 0.02;  
+	double temperature = 0;  
 	
 	for(int index = 0; index < 21; ++index){
 		temperature = 0;
-		daneH = 0;   
-		daneL = 0; 
-		i2c_start_wait(adres+I2C_WRITE);
-		i2c_write(0x07);                     // Zapis warto?ci 0x07 (wybranie rejestru Tobj1)
-		i2c_rep_start(adres+I2C_READ);
-		daneL = i2c_readAck();               // Odczyt pierwszego bajtu danych
-		daneH = i2c_readAck();               // Odczyt drugiego bajtu danych
-		erc = i2c_readNak();                 // Odczyt trzeciego (nieistotnego) bajtu danych
+		dateH = 0;   
+		dataL = 0; 
+		i2c_start_wait(adress+I2C_WRITE);
+		i2c_write(0x07);                     
+		i2c_rep_start(adress+I2C_READ);
+		dataL = i2c_readAck();           
+		dateH = i2c_readAck();             
+		erc = i2c_readNak();                
 		i2c_stop();
 		
-		temperature = (double)(((daneH & 0x007F) << 8) + daneL); // Utworzenie 16-sto bitowej zmiennej sk?adaj?cej si? z dwóch zmiennych jedno-bajtowych
-		temperature = temperature*tempnalsb; // Na jeden bit przypada 0.02 K, wynikiem tej operacji jest temperatura w Kelvinach
+		temperature = (double)(((dateH & 0x007F) << 8) + dataL); 
+		temperature = temperature*tempnalsb; 
 
-		temperature = temperature - 273.15; // Konwersja na stopnie Celsiusza
+		temperature = temperature - 273.15; 
 		Temperatures[index] = temperature;
 		
 	}
 	
 	
 }
-int cmp(const void *x, const void *y)
-{
-	double xx = *(double*)x, yy = *(double*)y;
-	if (xx < yy) return -1;
-	if (xx > yy) return  1;
-	return 0;
+
+
+double approximation(double temp){
+
+	return temp - (0.0038*temp*temp + -0.3371 * temp + 7.7529);
 }
-		
-void dataProcesing(double Temperatures[21], char *average, char *min,char *max){
+	
+void dataProcesing(double Temperatures[21], char *average){
 		
 	
 		double sort[21];
@@ -134,24 +135,11 @@ void dataProcesing(double Temperatures[21], char *average, char *min,char *max){
 			sort[x] = Temperatures[x];
 			averageTMP += Temperatures[x];
 		}
-		
-		
-
+			
 		averageTMP = averageTMP/20;
-		
-		for(int x = 0; x < 21; ++x)
-			for(int y = 0; y < 20; ++y){
-				if(Temperatures[y] > Temperatures[y+1]){
-					double tmp = Temperatures[y];
-					Temperatures[y] = Temperatures[y + 1];
-					Temperatures[y + 1]  = tmp;
-				}
-		}
-
-	
+		averageTMP = approximation(averageTMP);
 		convertDoubleToCharDisplay(averageTMP,average);
-		convertDoubleToCharDisplay(sort[0],min);
-		convertDoubleToCharDisplay(sort[20],max);
+
 	 
 }
 
@@ -176,12 +164,12 @@ int main(void)
 		if(!(PIND & _BV(PIND7)))
 		{
 			getData(Temperatures);
-			dataProcesing(Temperatures,average,min,max);
+			dataProcesing(Temperatures,average);
 			_delay_ms(20);
 		}
 
 	
-		displayTemperature(1,average,min,max);
+		displayTemperature(1,average);
 		_delay_ms(200);
 
 	}
